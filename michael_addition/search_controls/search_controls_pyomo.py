c0d33cb56783d3a6f4e14ddef71b09614c4f01fe
KennedyPutraKusumo <kennedy.putra.kusumo@gmail.com>
2022-04-05 16:00:44 +0100

    added multistart optimization for scipy solvers started pyomo solvers

---
+++ michael_addition/search_controls/search_controls_pyomo.py
@@ -0,0 +1,153 @@
+from pyomo import environ as po
+from oop_classes.point_normalizer import Normalizer
+import numpy as np
+
+
+def create_model():
+    model = po.ConcreteModel()
+
+    model.tau = po.Var(bounds=(0, None))
+
+    # set of chemical species (in order of appearance in Laky 2018 paper)
+    model.i = po.Set(initialize=([
+        "AH",
+        "B",
+        "A-",
+        "BH+",
+        "C",
+        "AC-",
+        "P",
+    ]))
+    # set of chemical reactions (in order of appearance in Laky 2018 paper)
+    model.j = po.Set(initialize=([
+        1,
+        2,
+        3,
+        4,
+        5,
+    ]))
+
+    model.c = po.Var(model.i, within=po.NonNegativeReals)
+    model.c_in = po.Var(model.i)
+
+    k = {
+        1: 49.7796,
+        2:  8.9316,
+        3:  1.3177,
+        4:  0.3109,
+        5:  3.8781,
+    }
+    model.k = po.Param(model.j, initialize=k)
+
+    nu = {
+        ("AH", 1): -1,
+        ("AH", 2):  0,
+        ("AH", 3):  0,
+        ("AH", 4): -1,
+        ("AH", 5):  0,
+
+        ("B", 1): -1,
+        ("B", 2):  0,
+        ("B", 3):  0,
+        ("B", 4):  0,
+        ("B", 5):  1,
+
+        ("A-", 1):  1,
+        ("A-", 2): -1,
+        ("A-", 3):  1,
+        ("A-", 4):  1,
+        ("A-", 5):  0,
+
+        ("BH+", 1):  1,
+        ("BH+", 2):  0,
+        ("BH+", 3):  0,
+        ("BH+", 4):  0,
+        ("BH+", 5): -1,
+
+        ("C", 1):  0,
+        ("C", 2): -1,
+        ("C", 3):  1,
+        ("C", 4):  0,
+        ("C", 5):  0,
+
+        ("AC-", 1):  0,
+        ("AC-", 2):  1,
+        ("AC-", 3): -1,
+        ("AC-", 4): -1,
+        ("AC-", 5): -1,
+
+        ("P", 1):  0,
+        ("P", 2):  0,
+        ("P", 3):  0,
+        ("P", 4):  1,
+        ("P", 5):  1,
+    }
+    model.nu = po.Param(model.i, model.j, initialize=nu)
+
+    def _mass_bal(m, i):
+        r = {
+            1: m.k[1] * m.cqa["AH"] * m.cqa["B"],
+            2: m.k[2] * m.cqa["A-"] * m.cqa["C"],
+            3: m.k[3] * m.cqa["AC-"],
+            4: m.k[4] * m.cqa["AC-"] * m.cqa["AH"],
+            5: m.k[5] * m.cqa["AC-"] * m.cqa["BH+"],
+        }
+        return m.c_in[i] - m.cqa[i] + m.tau * sum(m.nu[i, j] * r[j] for j in m.j) == 0
+    model.mass_bal = po.Constraint(model.i, rule=_mass_bal)
+
+    def _cqa_cons_1(m):
+        return m.cqa["C"] + m.cqa["AC-"] - 0.1 * m.c_in["C"] <= 0
+    model.cqa_cons_1 = po.Constraint(rule=_cqa_cons_1)
+
+    def _cqa_cons_2(m):
+        return m.cqa["AC-"] <= 0.002
+    model.cqa_cons_2 = po.Constraint(rule=_cqa_cons_2)
+
+    def _dummy_obj(m):
+        return 0
+    model.dummy_obj = po.Objective(rule=_dummy_obj, sense=po.maximize)
+
+    return model
+
+def maximal_spread(x):
+    n_trials = int(x.size/2)
+    x = x.reshape((n_trials, 2))
+
+    model = create_model()
+
+    model.j = po.Set(np.arange(0, n_trials))
+
+    model.c_in["AH"].fix(0.3955)
+    model.c_in["C"].fix(0.25)
+    model.c_in["BH+"].fix(0)
+    model.c_in["A-"].fix(0)
+    model.c_in["AC-"].fix(0)
+    model.c_in["P"].fix(0)
+
+    cqa1 = po.Var(model.i, model.i)
+    cqa2 = po.Var(model.i, model.i)
+
+    eta = po.Var()
+
+    def _cqa1_def(m):
+        return m.cqa1 == m.c["C"] + m.c["AC-"] - 0.1 * m.c_in["C"]
+    cqa1_def = po.Constraint(rule=_cqa1_def)
+
+    def _cqa2_def(m):
+        return m.cqa2 == 0.002 - po.value(model.c["AC-"])
+    cqa2_def = po.Constraint(rule=_cqa2_def)
+
+    def _minimal_distance_def(m, i, j):
+        if i >= j:
+            return m.eta <= (m.cqa1[i, j] - m.cqa2[i, j]) ** 2
+        else:
+            po.Constraint.Skip()
+    minimal_distance_def = po.Constraint(model.j, model.j, rule=_minimal_distance_def)
+
+    for run in x:
+        tau = x[0]
+        R = x[1]
+        model.tau.fix(tau)
+        model.c_in["B"].fix(0.3955 / R)
+
+    return
---
+++ michael_addition/search_controls/search_controls_scipy.py
@@ -0,0 +1,117 @@
+from michael_addition.model_scipy import multvar_sim_cqa
+from oop_classes.point_normalizer import Normalizer
+from scipy.optimize import minimize
+from scipy.spatial.distance import cdist
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def maximal_spread(x):
+    x = x.reshape(int(x.size/2), 2)
+    cqas = multvar_sim_cqa(x)
+    pi_pj = cdist(cqas, cqas)
+    spread = pi_pj[~np.eye(*pi_pj.shape, dtype=bool)].min()
+    return -spread
+
+def orthogonality(x):
+    x = x.reshape(int(x.size/2), 2)
+    cqas = multvar_sim_cqa(x)
+    norm1 = Normalizer(cqas)
+    cqas = norm1.normalize()
+    # append column of 1
+    cqas = np.append(np.ones((cqas.shape[0], 1)), cqas, axis=1)
+    info_matrix = cqas @ cqas.T
+    return -np.linalg.det(info_matrix)
+
+
+if __name__ == '__main__':
+    n_trials = 4
+    n_seeds = 10
+    spread_results = []
+    orthogonality_results = []
+
+    fig1 = plt.figure()
+    fig2 = plt.figure()
+
+    # multistart optimization
+    for seed in range(n_seeds):
+        print(f"Running seed {seed}")
+        np.random.seed(seed)
+        x0 = np.random.uniform(
+            low=[10, 400],
+            high=[30, 1400],
+            size=(n_trials, 2),
+        )
+        # x0 = np.array([
+        #     [10,  400],
+        #     [10, 1400],
+        #     [30,  400],
+        #     [30, 1400],
+        # ])
+        # Maximal Spread Design
+        opt_result = minimize(
+            maximal_spread,
+            x0=x0,
+            method="l-bfgs-b",
+            bounds=[
+                (10, 30),
+                (400, 1400),
+            ] * n_trials,
+            options={
+                "disp": True,
+            },
+            tol=1e-8,
+        )
+        print("Maximal Spread")
+        print(opt_result.x)
+        print(opt_result.fun)
+        spread_results.append([opt_result.x, -opt_result.fun])
+
+        # Maximal Orthogonality Design
+        opt_result2 = minimize(
+            orthogonality,
+            x0=x0,
+            method="l-bfgs-b",
+            bounds=[
+                       (10, 30),
+                       (400, 1400),
+                   ] * n_trials,
+            options={
+                "disp": True,
+            },
+            tol=1e-8,
+        )
+        print("Maximal Orthogonality")
+        print(opt_result2.x)
+        print(opt_result2.fun)
+        orthogonality_results.append(
+            [opt_result2.x, -opt_result2.fun]
+        )
+    spread_results = np.array(spread_results)
+    best_spread_design_idx = np.argmax(spread_results[:, 1], axis=0)
+    x = opt_result.x.reshape(n_trials, 2)
+    axes1 = fig1.add_subplot(121)
+    axes1.scatter(
+        x[:, 0],
+        x[:, 1],
+    )
+    cqas = multvar_sim_cqa(x)
+    axes2 = fig1.add_subplot(122)
+    axes2.scatter(
+        cqas[:, 0],
+        cqas[:, 1],
+    )
+
+    x = opt_result2.x.reshape(n_trials, 2)
+    axes3 = fig2.add_subplot(121)
+    axes3.scatter(
+        x[:, 0],
+        x[:, 1],
+    )
+    cqas = multvar_sim_cqa(x)
+    axes4 = fig2.add_subplot(122)
+    axes4.scatter(
+        cqas[:, 0],
+        cqas[:, 1],
+    )
+    plt.show()
